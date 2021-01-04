85843d20855f9f44b8e08b32ebd81026ddbb4ef8
rlnd-ldwg <zkh.60438@gmail.com>
2021-01-04 15:43:50 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# avr toolchain
-
 : ${AVR_DIR:="/usr/local/avr"}
 : ${AVR_GCC_DIR:="/usr/local/lib/gcc/avr"}
 
@@ -36,34 +34,3 @@ __EOF__
 cat <<__EOF__ >.piopm
 {"type": "tool", "name": "$DESC", "version": "$VERSION", "spec": {"owner": "platformio", "id": 8203, "name": "$DESC", "requirements": null, "url": null}}
 __EOF__
-
-
-# avrdude
-
-: ${AVRDUDE:="/usr/local/bin/avrdude"}
-: ${AVRDUDE_CONF:="/usr/local/etc/avrdude.conf"}
-
-VERSION="1.60300.200527"
-DESC="tool-avrdude"
-REPO_NAME="platformio-freebsd-${DESC}"
-REPO_OWNER="rlnd-ldwg"
-
-mkdir bin
-ln -s ${AVRDUDE} bin/avrdude
-
-cat <<__EOF__ >package.json
-{
-    "description": "${DESC}",
-    "name": "${DESC}",
-    "system": [
-        "${OS}_${ARCH}"
-    ],  
-    "url": "https://github.com/${REPO_OWNER}/${REPO_NAME}",
-    "version": "${VERSION}"
-}
-__EOF__
- 
-cat <<__EOF__ >.piopm
-{"type": "tool", "name": "$DESC", "version": "$VERSION", "spec": {"owner": "platformio", "id": , "name": "$DESC", "requirements": null, "url": null}}
-__EOF__
-
